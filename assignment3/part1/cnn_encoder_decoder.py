30b6e05469347890dae442eff405fb437c0c057c
Thies <100032503+thiesk@users.noreply.github.com>
2023-12-18 00:30:33 +0100

    Update cnn_encoder_decoder.py

--- assignment3/part1/cnn_encoder_decoder.py
+++ assignment3/part1/cnn_encoder_decoder.py
@@ -105,6 +105,7 @@ class CNNDecoder(nn.Module):
         # PUT YOUR CODE HERE  #
         #######################
         c_hid = num_filters
+        act_fn = nn.GELU
         self.linear = nn.Sequential(nn.Linear(z_dim, 2 * 16 * c_hid), nn.GELU())
         self.net = nn.Sequential(   
             nn.ConvTranspose2d(2*c_hid, 2*c_hid, kernel_size=3, output_padding=1, padding=1, stride=2),

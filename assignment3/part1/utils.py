feb2049b49255d0540f09ab80604b814f74896e8
thies <thies.kersten@gmail.com>
2023-12-19 16:39:24 +0100

    1.8

--- assignment3/part1/utils.py
+++ assignment3/part1/utils.py
@@ -67,10 +67,10 @@ def KLD(mean, log_std):
     # implementing formula from the pdf
 
     # part inside the sum
-    summand = 0.5 * ((2 * log_std).exp() + mean.pow(2) - 1 - 2 * log_std)
+    summand = 0.5 * ((2 * log_std).exp() + mean ** 2 - 1 - 2 * log_std)
 
     # sum and dividing by 2
-    KLD = torch.sum(input=summand, dim=1)
+    KLD = torch.sum(input=summand, dim=-1)
 
     #######################
     # END OF YOUR CODE    #
@@ -115,24 +115,42 @@ def visualize_manifold(decoder, grid_size=20):
     Outputs:
         img_grid - Grid of images representing the manifold.
     """
+    #######################
+    # PUT YOUR CODE HERE  #
+    #######################
 
     ## Hints:
     # - You can use the icdf method of the torch normal distribution  to obtain z values at percentiles.
     # - Use the range [0.5/grid_size, 1.5/grid_size, ..., (grid_size-0.5)/grid_size] for the percentiles.
+    percentiles = [(i-0.5) / grid_size for i in range(1, grid_size + 1)]
     # - torch.meshgrid might be helpful for creating the grid of values
-    # - You can use torchvision's function "make_grid" to combine the grid_size**2 images into a grid
-    # - Remember to apply a softmax after the decoder
+    x_latent, y_latent = torch.meshgrid(percentiles, percentiles, indexing="xy")
+    normal_dist = torch.distributions.Normal(0, 1)
+    z_1 = normal_dist.icdf(x_latent)
+    z_2 = normal_dist.icdf(y_latent)
 
-    #######################
-    # PUT YOUR CODE HERE  #
-    #######################
-    
-    # put channel to last dim
-    img = torch.permute(img, (0, 2, 3, 1))
-    # flatten img
-    
-    img_grid = None
-    raise NotImplementedError
+    # put together for make grid
+    z = torch.stack([z_1, z_2], dim=-1)
+    z = torch.flatten(z, end_dim=-2)
+
+    logit_z = decoder(z) # see what i did there
+
+    # - Remember to apply a softmax after the decoder
+    # get probs from logits
+    prob_z = torch.nn.functional.softmax(logit_z, dim=1)
+    # sort, st. they work with the categorical sampling
+    prob_z = torch.permute(prob_z, (0, 2, 3, 1))
+    prob_z = torch.flatten(prob_z, end_dim=2)
+
+    # sample categorically
+    imgs = torch.multinomial(prob_z, 1)
+    # put back to image batch format
+    imgs = imgs.reshape(-1, 28, 28, 1)
+    imgs = torch.permute(imgs, (0, 3, 1, 2))
+
+    # - You can use torchvision's function "make_grid" to combine the grid_size**2 images into a grid
+    img_grid = make_grid(imgs, nrow=grid_size).float()
+
     #######################
     # END OF YOUR CODE    #
     #######################
